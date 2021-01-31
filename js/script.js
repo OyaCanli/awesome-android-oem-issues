f706e3781bdd00bb8a27184d23f3e721a8fc9bce
MaximeFERRIER <maxime.ferrier@gmx.com>
2021-01-31 19:55:31 +0100

    Sort data

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $().ready(function(){
     $.getJSON("./data/issues.json", function(data) {
 
-        //TODO : Sort by topic; and in topics sort by relatedClass
         var theme = './theme/default.mst';
+
+        data = data.sort(sortByProperty("topic"))
+
+        for(var topic in data) {
+            data[topic].issues = data[topic].issues.sort(sortByProperty("relatedClass"))
+        }
 
         $.get(theme, function(template) {
             Mustache.parse(template); 
@@ -13,4 +18,15 @@ $().ready(function(){
     }).fail(function(){
         console.log("An error has occurred.");
     });
+
+    function sortByProperty(property){  
+        return function(a,b){  
+           if(a[property] > b[property])  
+              return 1;  
+           else if(a[property] < b[property])  
+              return -1;  
+       
+           return 0;  
+        }  
+     }
 });
